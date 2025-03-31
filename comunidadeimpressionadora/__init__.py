e063790e7201499adac83ef652fe6b467f109cd2
robdantas84 <106701708+robdantas84@users.noreply.github.com>
2025-03-31 11:35:59 -0400

    Add files via upload

---
+++ comunidadeimpressionadora/__init__.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+
+
+SECRET_KEY = os.urandom(6)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
+
+
+from comunidadeimpressionadora import routes
+
+
+
+
